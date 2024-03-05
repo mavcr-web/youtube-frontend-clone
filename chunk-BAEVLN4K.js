eb09fcab8cbee17bc6894e9b2cef33085f497e58
mavcr <mianvacrro@gmail.com>
2024-03-05 20:09:42 +0000

    Auto-generated commit

--- chunk-BAEVLN4K.js
+++
@@ -1,1 +0,0 @@
-import{Ca as i,Da as o,Ga as a,Ja as s,P as r}from"./chunk-XUATWZNQ.js";var d=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=r({type:e,selectors:[["app-register"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,c){n&1&&(i(0,"p"),a(1,"register works!"),o())}});let t=e;return t})();export{d as RegisterComponent};
