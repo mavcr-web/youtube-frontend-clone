894d1b90f9790a9b3f9fa524e853105880f12862
mavcr <mianvacrro@gmail.com>
2024-03-05 18:14:31 +0000

    Add Angular Material and update styles

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -5,11 +5,12 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { jwtInterceptor } from './core/interceptors/jwt.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(withInterceptors([jwtInterceptor]),withFetch()),
+    provideHttpClient(withInterceptors([jwtInterceptor]),withFetch()), provideAnimationsAsync(),
   ],
 };
