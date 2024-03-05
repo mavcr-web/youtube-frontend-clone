2ececa918c359863eb2e4374eb5c42e8fae896e4
mavcr <mianvacrro@gmail.com>
2024-03-05 18:23:29 +0000

    Fix environment import and remove unused files

--- frontend/src/app/video/services/video.service.ts
+++ frontend/src/app/video/services/video.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 
-import { environment } from '../../../environment/environment';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
