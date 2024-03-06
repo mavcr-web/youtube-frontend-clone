7fa113f648aa03cf00699722ef813bdd116954f1
mavcr <mianvacrro@gmail.com>
2024-03-06 03:02:28 +0000

    Add login form and authentication functionality

--- frontend/src/app/auth/pages/login/login.component.ts
+++ frontend/src/app/auth/pages/login/login.component.ts
@@ -1,12 +1,55 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-
+  loginForm!: FormGroup;
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private _router: Router
+  ) {}
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+  login() {
+    const body = this.loginForm.value;
+    this.authService.login(body).subscribe((data) => {
+      sessionStorage.setItem('token', data.token);
+      sessionStorage.setItem('role', data.role);
+      this._router.navigate(['/']);
+    });
+  }
+  resetLoginForm() {
+    this.loginForm.reset();
+  }
+  focus(input: HTMLElement) {
+    input.focus();
+  }
 }
--- frontend/src/app/auth/services/auth.service.ts
+++ frontend/src/app/auth/services/auth.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  url = environment.url;
 
-  constructor() { }
+  private _httpClient: HttpClient = inject(HttpClient);
+
+  login(body: { username: string; password: string }) {
+    return this._httpClient.post<{ token: string; role: string }>(
+      `${this.url}/auth/login`,
+      body
+    );
+  }
+
+  register(body: { username: string; password: string }) {
+    return this._httpClient.post(`${this.url}/auth/register`, body);
+  }
+
+  changePassword(password: string) {
+    return this._httpClient.put<{ action: string }>(
+      `${this.url}/change-password`,
+      {
+        password,
+      }
+    );
+  }
 }
