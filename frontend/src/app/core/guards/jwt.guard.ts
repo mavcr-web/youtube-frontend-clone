11b99db8aee6badc64bc5c03ab09b7998985274a
mavcr <mianvacrro@gmail.com>
2024-03-07 07:42:10 +0000

    Fix route guards order in app.routes.ts and remove unnecessary code in sidebar.component.html

--- frontend/src/app/core/guards/jwt.guard.ts
+++ frontend/src/app/core/guards/jwt.guard.ts
@@ -1,12 +1,9 @@
-import { afterNextRender } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 
 export const jwtGuard: CanActivateFn = (route, state) => {
   let token = '';
 
-  afterNextRender(() => {
-    token = sessionStorage.getItem('token') || '';
-  });
+  token = sessionStorage.getItem('token') || '';
 
   if (!token || token === '') {
     return false;
